4888e6a453d9c03c7e9adb16f8e7ee322f4dc33a
Yinkang <Li.Yinkang@ontoinnovation.com>
2024-05-21 16:31:27 +0800

    Added collision and scoreboard

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.locals import *
 import time
+import random
 
 SIZE = 40
 class Apple:
@@ -13,6 +14,11 @@ class Apple:
     def draw(self):
         self.parent_screen.blit(self.image, (self.x, self.y))
         pygame.display.flip()
+    
+    
+    def move(self):
+        self.x = random.randint(0, 24)*SIZE
+        self.y = random.randint(0, 19)*SIZE
 
 class Snake:
     def __init__(self, parent_screen, length):
@@ -22,6 +28,11 @@ class Snake:
         self.y = [SIZE]*length
         self.parent_screen = parent_screen
         self.direction = "down"
+
+    def increase_length(self):
+        self.length += 1
+        self.x.append(-1)
+        self.y.append(-1)
     
     def draw(self):
         self.parent_screen.fill((0, 0, 0))
@@ -60,16 +71,41 @@ class Snake:
 class Game:
     def __init__(self):
         pygame.init()
-        self.surface = pygame.display.set_mode((1000, 1000))
+        self.surface = pygame.display.set_mode((1000, 800))
         self.surface.fill((0, 0, 0))
-        self.snake = Snake(self.surface, 6)
+        self.snake = Snake(self.surface, 1)
         self.snake.draw()
         self.apple = Apple(self.surface)
         self.apple.draw()
 
+    def is_collision(self, x1, y1, x2, y2):
+        # x1, y1 is the top left edge of the snake while x2, y2 is the top left edge of the apple
+        # x1 >= x2 checks for condition when the snake collides the apple from the left
+        # x1 <= x2 + SIZE checks for condition when the snake collides the apple from the right
+        # y1 >= y2 checks for condition when the snake collides the apple from the top
+        # y1 <= y2 + SIZE checks for condition whe the snake collides the apple from the bottom
+
+        if x1 >= x2 and x1 < x2 + SIZE:
+            if y1 >= y2 and y1 < y2 + SIZE:
+                return True
+        return False
+
+
     def play(self):
         self.snake.walk()
         self.apple.draw()
+        self.display_score()
+        pygame.display.flip()
+        
+        if self.is_collision(self.snake.x[0], self.snake.y[0], self.apple.x, self.apple.y):
+            print("collide")
+            self.snake.increase_length()
+            self.apple.move()
+
+    def display_score(self):
+        font = pygame.font.SysFont("arial", 30)
+        score = font.render(f"Score: {self.snake.length}", True, (255, 255, 255))
+        self.surface.blit(score, (800, 10))
 
     def run(self):
         running = True
@@ -90,7 +126,7 @@ class Game:
                     running = False
 
             self.play()
-            time.sleep(0.3) 
+            time.sleep(0.2) 
 
 
 
