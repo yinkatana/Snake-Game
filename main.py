2ad23d35d2ee90742db5f2f238d713ca235ce08c
Yinkang <Li.Yinkang@ontoinnovation.com>
2024-05-21 12:21:06 +0800

    Moving blocks created

---
+++ main.py
@@ -0,0 +1,43 @@
+import pygame
+from pygame.locals import *
+
+import pygame.docs
+
+
+def draw_block():
+    surface.fill((255, 255, 255))
+    surface.blit(block, (block_x, block_y))
+    pygame.display.flip()
+
+if __name__ == "__main__":
+    pygame.init()
+
+    surface = pygame.display.set_mode((1000, 500))
+    surface.fill((255,255,255))
+
+    block = pygame.image.load("block.jpg").convert()
+    block_x = 100
+    block_y = 100
+    surface.blit(block, (block_x, block_y))
+    pygame.display.flip()
+    
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    running = False
+                if event.key == K_UP:
+                    block_y -= 10
+                    draw_block()
+                if event.key == K_DOWN:
+                    block_y += 10
+                    draw_block()
+                if event.key == K_LEFT:
+                    block_x -= 10
+                    draw_block()
+                if event.key == K_RIGHT:
+                    block_x += 10
+                    draw_block()
+            elif event.type == QUIT:
+                running = False
